51b922a26b398138e4a52d24f5b631d89e06c10f
cqlboy <cqlboy@hotmail.com>
2019-10-06 15:09:13 -0700

    TIME CHANGE:  3:08

--- ADCHelloWorld.java
+++ ADCHelloWorld.java
@@ -1,6 +1,6 @@
 public class ADCHelloWorld {
 	public static void main(String[] args) {
 		// Prints "Hello, World" in the terminal window.
-		System.out.println("TIME CHANGE:  11:26");
+		System.out.println("TIME CHANGE:  3:08");
 	}
 }
